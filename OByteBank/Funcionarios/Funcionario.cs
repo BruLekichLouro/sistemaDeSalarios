95f8a82a371124f9276974526c65dcb47f8c6f1e
BruLekichLouro <bububio@yahoo.com.br>
2018-09-04 13:14:10 -0300

    Adicionando outra parte dos exercícios

---
+++ OByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public Funcionario(double salario, string cpf)
+        {
+            Console.WriteLine("Criando FUNCIONARIO");
+            TotalDeFuncionarios++;
+            CPF = cpf;
+            Salario = salario;
+        }
+
+        public virtual void AumentarSalario()
+        {
+            Salario *= 1.1;
+        }
+
+        public virtual double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ OByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using OByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Designer pedro = new Designer("833.222.048-39");
+            pedro.Nome = "Pedro";
+
+            Funcionario roberta = new Diretor("159.753.398-04");
+            roberta.Nome = "Roberta";
+
+            Auxiliar igor = new Auxiliar("981.198.778-53");
+            igor.Nome = "Igor";
+
+            GerenteDeConta camila = new GerenteDeConta("326.985.628-89");
+            camila.Nome = "Camila";
+
+            gerenciadorBonificacao.Registrar(pedro);
+            gerenciadorBonificacao.Registrar(roberta);
+            gerenciadorBonificacao.Registrar(igor);
+            gerenciadorBonificacao.Registrar(camila);
+
+            Console.WriteLine("Total de bonificações do mês " +
+                gerenciadorBonificacao.GetTotalBonificacao());
+
+
+            Console.ReadLine();
+        }
+    }
+}
